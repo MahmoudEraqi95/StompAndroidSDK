09faec665eaaa48eae94a62c36fb233513d3ec15
Eraqi <mahmouderaqi225@yahoo.com>
2022-09-10 14:11:27 +0200

    updated to v.06

--- chatlib/src/main/java/com/eraqi/chatlib/utils/Logger.kt
+++ chatlib/src/main/java/com/eraqi/chatlib/utils/Logger.kt
@@ -1,4 +1,13 @@
 package com.eraqi.chatlib.utils
 
+import android.util.Log
+
 class Logger {
+    private val TAG = "stompkt"
+    fun logInfo(message: String){
+        Log.i(TAG, message)
+    }
+    fun logError(throwable: Throwable){
+        Log.e(TAG, throwable.message, throwable)
+    }
 }
